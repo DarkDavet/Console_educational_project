e92b28b5121b3bf949871358bc26103aed56733f
DarkDavet <darkdavet@gmail.com>
2026-05-16 15:05:37 +0300

    added StringsOperation

---
+++ StringsOperation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_educational_project
+{
+    internal class StringsOperation
+    {
+        public void Interpolate_1()
+        {
+            int score = 10;
+            // было(С++):    sprintf(buf, "Score: %d", score)
+
+            /* стало(С#):*/
+            string message = $"Score: {score}";   // знак $ перед кавычками делает интерполяцию возможной
+
+            Console.WriteLine(message);
+        }
+    }
+}
