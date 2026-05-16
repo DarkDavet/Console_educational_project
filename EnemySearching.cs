760d8e982607d588320f581e85894d078376abc3
DarkDavet <darkdavet@gmail.com>
2026-05-16 15:00:57 +0300

    added EnemySearching

---
+++ EnemySearching.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_educational_project
+{
+    internal class EnemySearching
+    {
+        private double[] enemiesHealth = { 50.3, 3, 80, 5, 100, 20 };
+
+        public void FindEnemy()
+        {
+            Console.WriteLine("Враги с малым количеством здоровья (HP меньше 20):");
+            foreach (int hp in enemiesHealth)
+            {
+                if (hp < 20)
+                {
+                    Console.WriteLine("Враг найден! HP: " + hp);
+                }
+
+            }
+        }
+    }
+}
