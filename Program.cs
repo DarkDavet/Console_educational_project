11646c294045e834e3628368609d058b2ee0deec
DarkDavet <darkdavet@gmail.com>
2026-05-16 14:48:01 +0300

    added UTF8

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
-﻿namespace Console_educational_project
+﻿using System.Text;
+
+namespace Console_educational_project
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+            }
             Console.WriteLine("Hello, World!");
         }
     }
