4bf4b436910720d21009c4a2a11648868706f92a
DarkDavet <darkdavet@gmail.com>
2026-05-30 13:40:29 +0300

    created CheckTraining, added Check_1

---
+++ CheckTraining.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_educational_project
+{
+    public class CheckTraining
+    {
+        public void Check_1()
+        {
+            int level = 10;
+            int nextLevel = level;
+            nextLevel = 20;
+
+            Console.WriteLine(level); // Что выведет здесь?
+        }
+    }
+}
