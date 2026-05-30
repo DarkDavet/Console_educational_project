d6779cce361c3b4310264c792fd9e9e6bfee06bc
DarkDavet <darkdavet@gmail.com>
2026-05-30 13:43:38 +0300

    added Check_2

--- CheckTraining.cs
+++ CheckTraining.cs
@@ -15,6 +15,18 @@ namespace Console_educational_project
             nextLevel = 20;
 
             Console.WriteLine(level); // Что выведет здесь?
+
+            // Суть: В C# int — это «значимый» тип.
+            // Когда мы пишем nextLevel = level, создается полная копия числа.
+            // Изменение копии не трогает оригинал.
+        }
+
+        public void Check_2()
+        {
+            string heroName = "геральт";
+            heroName.ToUpper(); // Метод делает строку заглавными буквами
+
+            Console.WriteLine(heroName); // Что выведет здесь?
         }
     }
 }
