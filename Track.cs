0922ecd93a01cb8613be8b127cabbba393317a77
MaSe <misaboldovskij@gmail.com>
2026-06-06 15:35:53 +0300

    updated

--- Track.cs
+++ Track.cs
@@ -6,19 +6,25 @@ using System.Threading.Tasks;
 
 namespace Console_educational_project
 {
-    public class Track
-    {
-        public string Title { get; set; }     // свойства
-        public string Artist { get; set; }    // позволяют хранить в себе значения, как поля,
-                                              // но с использованием модификаторов доступа
+   public class Track
+{
 
-        public Track(string title, string artist)     // конструктор: специальный метод,
-                                                      // который срабатывает один раз при создании объекта этого класса
-                                                      // называется обязательно именем этого класса
-        {
-            Title = title;
-            Artist = artist;
-        }
+    private string _title;
+    private string _artist;
 
+    public string Title 
+    { 
+        get { return _title; } 
     }
+    public string Artist 
+    { 
+        get { return _artist; } 
+    }
+
+    public Track(string title, string artist)
+    {
+        _title = title;
+        _artist = artist;
+    }
+}
 }
