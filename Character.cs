651065f1ce0e48562df62bb15b74ef698bdb348f
DarkDavet <darkdavet@gmail.com>
2026-06-06 15:38:05 +0300

    added task_2

---
+++ Character.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_educational_project
+{
+    /*
+    Задание 2: «Создание персонажа для RPG»
+    Напиши код для создания персонажа в игре, используя свойства и конструкторы 
+    У каждого героя должно быть имя, уровень и количество здоровья (HP). 
+    Здоровье персонажа не может быть меньше нуля, а уровень героя при создании всегда 1-й.
+
+     */
+    public class Character
+    {
+    }
+}
