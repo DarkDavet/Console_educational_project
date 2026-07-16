d77c92f75963971c03cedbb8874fc9423b82940a
MaSe <misaboldovskij@gmail.com>
2026-07-16 16:16:43 +0300

    updated

--- ProductCatalog.cs
+++ ProductCatalog.cs
@@ -8,6 +8,11 @@ namespace Console_educational_project
 {
      public class ProductCatalog
      {
-        private List<Product> productList = new List<Product>();
+        private List<Product> shopCart = new List<Product>
+        {
+            new Product { Name = "Ноутбук", Price = 70000, Category = "Электроника" },
+            new Product { Name = "Мышка", Price = 3000, Category = "Аксессуары" },
+            new Product { Name = "Клавиатура", Price = 5000, Category = "Аксессуары" }
+        };
      }
 }
