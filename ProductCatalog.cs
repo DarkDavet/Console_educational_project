28608cf55e06cf327971009e6b84a4dfe1703c8c
DarkDavet <darkdavet@gmail.com>
2026-07-16 16:10:39 +0300

    added ProductCatalog with product list

---
+++ ProductCatalog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_educational_project
+{
+     public class ProductCatalog
+     {
+        private List<Product> productList = new List<Product>();
+     }
+}
