374a3abd8a7de7a4449d6e5f0c74e63785e3c13c
MaSe <misaboldovskij@gmail.com>
2026-08-06 17:03:42 +0300

    updated

--- Program.cs
+++ Program.cs
@@ -4,15 +4,15 @@ namespace Console_educational_project
 {
     internal class Program
     {
-        MethodsExecuter  welcomeMessege = new  MethodsExecuter();
-         welcomeMessege.CalculateSomething();
-          welcomeMessege.PrintWelcomeMessage
+       
         static void Main(string[] args)
         {
             {
                 Console.OutputEncoding = Encoding.UTF8;
             }
-            
+             MethodsExecuter  welcomeMessege = new  MethodsExecuter();
+         welcomeMessege.CalculateSomething();
+          welcomeMessege.PrintWelcomeMessage();
         }
     }
 }
