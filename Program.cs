f3deac7850022b3aceb37e6c2f9044b6848e6051
MaSe <misaboldovskij@gmail.com>
2026-08-06 16:43:29 +0300

    updated

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 namespace Console_educational_project
 {
@@ -6,49 +7,46 @@ namespace Console_educational_project
     {
         static void Main(string[] args)
         {
-            {
-                Console.OutputEncoding = Encoding.UTF8;
-            }
-             ProductCatalog catalog = new ProductCatalog(); 
+            Console.OutputEncoding = Encoding.UTF8;
             
-            catalog.PrintAllProducts(); 
-            catalog.TotalPrice();
-
+            ProductCatalog catalog = new ProductCatalog(); 
+            
             bool isRunning = true;
-             while (isRunning)
-        {
-            Console.WriteLine("\n Меню интернет-магазина ");
-            Console.WriteLine("1 — Показать товары");
-            Console.WriteLine("2 — Добавить товар");
-            Console.WriteLine("3 — Показать сумму");
-            Console.WriteLine("0 — Выход");
-            Console.Write("Выберите действие: ");
-
-            string input = Console.ReadLine();
-            
-
-            switch (input)
+            while (isRunning)
             {
-                case "1":
-                   
-                    catalog.PrintAllProducts(); 
-                    break;
-                case "2":
-                    catalog.AddProduct();
-                    break;
-                case "3":
-                   
-                    catalog.TotalPrice(); 
-                    break;
-                case "0":
-                    isRunning = false;
-                    Console.WriteLine("Программа завершена");
-                    break;
-                default:
-                    Console.WriteLine("Неверный ввод. выберите 1, 2, 3 или 0");
-                    break;
+                Console.WriteLine("\n Меню интернет-магазина ");
+                Console.WriteLine("1 — Показать товары");
+                Console.WriteLine("2 — Добавить товар");
+                Console.WriteLine("3 — Показать сумму");
+                Console.WriteLine("4 — Фильтрация по категории");
+                Console.WriteLine("0 — Выход");
+                Console.Write("Выберите действие: ");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        catalog.PrintAllProducts(); 
+                        break;
+                    case "2":
+                        catalog.AddProduct();
+                        break;
+                    case "3":
+                        catalog.TotalPrice(); 
+                        break;
+                    case "4":
+                        catalog.FilterCategory();
+                        break;
+                    case "0":
+                        isRunning = false;
+                        Console.WriteLine("Программа завершена");
+                        break;
+                    default:
+                        Console.WriteLine("Неверный ввод. Выберите 1, 2, 3, 4 или 0");
+                        break;
+                }
             }
-        }
         }
     }
 }
