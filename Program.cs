cfbc3ae6b1b2d433af36277c55fd3de03129660e
DarkDavet <darkdavet@gmail.com>
2026-08-27 16:52:52 +0300

    add a task

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ namespace Console_educational_project
 {
     internal class Program
     {
+        // Задание:
+        // Создать класс мага
+        // Добавить магу уникальное свойство Mana.
+        // В методе Main создать объекты Warrior и Wizard.
+        // Вызвать у обоих унаследованный метод ShowInfo().
         static void Main(string[] args)
         {
             {
@@ -12,6 +17,8 @@ namespace Console_educational_project
             }
 
             Warrior warrior_1 = new Warrior("11", 2, 3);
+            warrior_1.ShowInfo();
+            warrior_1.BlockAttack();
         }
     }
 }
