1601f40b794283072db5b220b70fbf20ce35d3b4
DarkDavet <darkdavet@gmail.com>
2026-08-27 16:05:34 +0300

    create Hero

---
+++ Hero.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_educational_project
+{
+
+    // БАЗОВЫЙ (РОДИТЕЛЬСКИЙ КЛАСС)
+    public class Hero
+    {
+        // Public: свойства видны везде (и во внешних классах, и в наследниках)
+        public string Name { get; set; }
+        public int Health { get; set; }
+
+        // Protected: скрыто от внешних классов, но доступно внутри классов-наследников
+        protected int baseSpeed = 10;
+
+        // Конструктор базового класса - срабатывает при создании объекта класса Hero
+        public Hero(string name, int health)
+        {
+            Name = name;
+            Health = health;
+        }
+
+        // Общий метод, который автоматически достанется всем потомкам
+        public void ShowInfo()
+        {
+            Console.WriteLine($"[Герой] Имя: {Name} | Здоровье: {Health} ОЗ | Базовая скорость: {baseSpeed}");
+        }
+    }
+}
