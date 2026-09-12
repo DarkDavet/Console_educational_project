9ad413738e1533f269aeeee7d2b65d70f1a63dec
DarkDavet <darkdavet@gmail.com>
2026-09-12 11:43:01 +0300

    add HybridCar class

---
+++ Vehicle_Task/HybridCar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_educational_project.Vehicle_Task
+{
+    // НАСЛЕДНИК 3 (Дополнительный класс для быстрой практики)
+    class HybridCar
+    {
+        public HybridCar(string model) : base(model) { }
+
+        // TODO: Переопредели метод StartEngine так, чтобы он:
+        // 1. Сначала вызвал базовый метод родителя (вывел "Двигатель заведен.")
+        // 2. А затем вывел свою уникальную строку: "[Model] переключился на электротягу."
+        public void StartEngine()
+        {
+       
+        }
+    }
+}
